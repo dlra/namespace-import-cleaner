51de17761cbc05dbaa9e73003993686c2a36a5ce
daniel_ansell_geeksltd <daniel.ansell@geeks.ltd.uk>
2021-03-12 12:02:28 +0000

    Add file cleaning service

---
+++ BadNamespaceImportRemover/Services/FileCleaningService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace BadNamespaceImportRemover.Services
+{
+    public class FileCleaningService
+    {
+        readonly string[] importsToRemove;
+        readonly string filePath;
+
+        public FileCleaningService(string filePath, params string[] importsToRemove)
+        {
+            this.filePath = filePath;
+            this.importsToRemove = importsToRemove;
+        }
+
+        public void CleanFile()
+        {
+            var text = File.ReadAllText(filePath);
+            var usingDirectivesToRemove = GetUsingDirectivesFromImportNames();
+
+            foreach (var usingDirective in usingDirectivesToRemove)
+            {
+                text = text.Replace(usingDirective, null);
+            }
+
+            File.WriteAllText(filePath, text);
+        }
+
+        string[] GetUsingDirectivesFromImportNames()
+        {
+            return importsToRemove.Select(str => GetUsingDirectiveFromImportName(str)).ToArray();
+        }
+
+        string GetUsingDirectiveFromImportName(string import)
+        {
+            return $"using {import};{Environment.NewLine}";
+        }
+    }
+}
