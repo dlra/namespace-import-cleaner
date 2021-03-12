019db2594d800de0e0f728166a8a94908800f7f3
daniel_ansell_geeksltd <daniel.ansell@geeks.ltd.uk>
2021-03-12 15:04:21 +0000

    Add file reading service

---
+++ BadNamespaceImportRemover/Services/FileReadingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace BadNamespaceImportRemover.Services
+{
+    public class FileReadingService
+    {
+        readonly string filePath;
+        readonly List<string> items = new List<string>();
+
+        public FileReadingService(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public IEnumerable<string> EnumerateLineItems()
+        {
+            string line;
+
+            using var stream = File.OpenRead(filePath);
+            using var reader = new StreamReader(stream);
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                items.Add(line);
+            }
+
+            return items;
+        }
+    }
+}
