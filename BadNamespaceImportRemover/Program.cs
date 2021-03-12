4515575a0d17f893f7b39d95290dee5e3c5998cd
daniel_ansell_geeksltd <daniel.ansell@geeks.ltd.uk>
2021-03-12 12:02:12 +0000

    Initial commit

---
+++ BadNamespaceImportRemover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadNamespaceImportRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
